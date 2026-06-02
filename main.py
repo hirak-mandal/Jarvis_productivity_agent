f572339d3bcdbab2b6c6cae9c026ba3559c0fe26
hirak <hirakmandal81@gmail.com>
2026-06-02 21:59:29 +0530

    backend central command & security
    Creatend FastAPI instance as the central command of backend architecture and used middleware & CORS to prevent same-origin policy error of Browser allowing websites to stream uninterruptedly.

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.chat import chat_router
+
+#The central command center of backend server
+app=FastAPI(
+    title="Jarvis_productivity_agent",
+    description="A productivity agent that helps you manage your tasks and schedule.",
+    version="1.0.0"
+)
+
+#security configurations: Cros-origin resource sharing
+#Frontend--> one path & Backend--> one path
+#can't talk due to Browsers same-origin policy
+#but middleware allows to talk to different origin websites
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #allow all websites to talk
+    allow_credentials=True, #allowing all cookies and sequrity check
+    allow_methods=["*"], #allow GET,POST,DELETE,UPDATE methods
+    allow_headers=["*"] #allow all headers
+)
+
+#sub-commands(routers) into the central hub
+app.include_router(chat_router)
