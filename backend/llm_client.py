41a8849b29d476d4a5f4baae25eafcab7e462aac
hirak <hirakmandal81@gmail.com>
2026-06-08 21:44:33 +0530

    AsyncGroq with token streaming

--- backend/llm_client.py
+++ backend/llm_client.py
@@ -1,10 +1,28 @@
 import os
-from groq import Groq
+import asyncio
+from groq import AsyncGroq
 from dotenv import load_dotenv
 
-load_dotenv()
-api_key=os.getenv("GROQ_API_KEY")
-if not api_key:
-    raise ValueError("API key not found!!!")
-client=Groq(api_key)
-model="llama-3.3-70b-versatile"
+async def main():
+    load_dotenv()
+    api_key=os.getenv("GROQ_API_KEY")
+    if not api_key:
+        raise ValueError("API key not found!!!")
+    client=AsyncGroq(api_key=api_key)
+    stream=await client.chat.completions.create(
+        messages=[
+            "role":"user",
+            "content":"You are a helpful assistant."
+        ],
+        model="llama-3.3-70b-versatile",
+        temperature=0.5,
+        max_completion_tokens=1024,
+        top_p=1,
+        stop=None,
+        stream=True
+    )    
+
+    async for chunk in stream:
+        print(chunk.choices[0].delta.content,end=" ")
+
+asyncio.run(main())
